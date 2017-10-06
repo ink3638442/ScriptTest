6be82c0eb6e178c3857bed9eb28a65e83cef86bf
AKIHIRO NAGASHIMA <akihiro.nagashima@gmail.com>
2017-10-07 00:07:01 +0900

    配列の課題

---
+++ Assets/Test.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		int[] array = {10, 20, 30, 40, 50};
+
+		Debug.Log("/////一覧表示/////");
+		for (int i = 0; i < array.Length; i++) {
+			Debug.Log(array[i]);
+		}
+
+		Debug.Log("/////逆順表示/////");
+		for (int i = array.Length - 1; i >= 0; i--) {
+			Debug.Log(array[i]);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
